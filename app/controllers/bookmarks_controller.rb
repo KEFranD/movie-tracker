d6037791acb5dca2a8c209e3a09de5ae66679140
Francesca D <130538538+KEFranD@users.noreply.github.com>
2023-10-02 15:55:20 +0400

    added bookmarks delete action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :set_list, only: %i[new create]
+  before_action :set_bookmark, only: %i[destroy]
 
   def new
     @list = List.find(params[:list_id])
@@ -16,10 +17,19 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
   private
 
   def set_list
     @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def bookmark_params
